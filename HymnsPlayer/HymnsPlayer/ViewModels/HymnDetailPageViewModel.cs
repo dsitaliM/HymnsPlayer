a6041462155b973af282990d22ad3a04e4a7ea98
Daniel Sitali <danielmsitali@outlook.com>
2018-09-14 13:17:41 +0200

    apparently setting ContentPage.BindingContext ruins everything. Removed the refrence and now I can navigate from DetailPage to PlayPage

--- HymnsPlayer/HymnsPlayer/ViewModels/HymnDetailPageViewModel.cs
+++ HymnsPlayer/HymnsPlayer/ViewModels/HymnDetailPageViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using HymnsPlayer.Models;
 using HymnsPlayer.Services;
-using Prism;
 using Prism.Navigation;
 
 namespace HymnsPlayer.ViewModels
@@ -13,17 +10,40 @@ namespace HymnsPlayer.ViewModels
 	public class HymnDetailPageViewModel : BindableBase, INavigatedAware
 	{
 
-	    private INavigationService _navigationService;
+	    private readonly INavigationService _navigationService;
+
 	    private Hymn _hymn;
 	    public Hymn Hymn
 	    {
 	        get => _hymn;
 	        set => SetProperty(ref _hymn, value);
 	    }
+
+//	    private DelegateCommand<Hymn> _playCommand;
+//	    public DelegateCommand<Hymn> PlayCommand
+//            => _playCommand ?? (_playCommand = new DelegateCommand<Hymn>(OnPlayClicked));
+
+	    private DelegateCommand _playCommand;
+	    public DelegateCommand PlayCommand
+	        => _playCommand ?? (_playCommand = new DelegateCommand(OnPlayClicked));
+
+//        private async void OnPlayClicked(Hymn hymn)
+//	    {
+//	        //var navParams = new NavigationParameters { { "HymnNymber", _hymn.HymnNumber } };
+//	        await _navigationService.NavigateAsync("MainNavigation?selectedTab=HymnPlayPage");
+//	    }
+
+	    private async void OnPlayClicked()
+	    {
+	        //var navParams = new NavigationParameters { { "HymnNymber", _hymn.HymnNumber } };
+	        await _navigationService.NavigateAsync("MainNavigation?selectedTab=HymnPlayPage");
+	    }
+
         public HymnDetailPageViewModel() { }
         public HymnDetailPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            //PlayCommand = new DelegateCommand(OnPlayClicked);
         }
 
 	    public void OnNavigatedFrom(INavigationParameters parameters)
@@ -33,14 +53,19 @@ namespace HymnsPlayer.ViewModels
 
 	    public void OnNavigatedTo(INavigationParameters parameters)
 	    {
-	        if (parameters.ContainsKey("HymnNumber"))
+	        var hymns = new HymnService();
+            if (parameters.ContainsKey("HymnNumber"))
 	        {
-	            var hymns = new HymnService();
 	            Hymn = hymns.Hymns.FirstOrDefault(hymn => hymn.HymnNumber == (int)parameters["HymnNumber"]);
 	        }
+            else
+            {
+                Hymn = hymns.Hymns.FirstOrDefault();
+            }
         }
 
-	    public void OnNavigatingTo(INavigationParameters parameters)
+	    
+        public void OnNavigatingTo(INavigationParameters parameters)
 	    {
 	        
         }
